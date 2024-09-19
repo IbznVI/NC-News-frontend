3f6fd0bda786b40c83a0c3f74738e1731d14422f
Ibrahim Ahmed <Ibrahimahmed2702@gmail.com>
2024-09-19 11:59:23 +0100

    added deleting comments and also added filtering by topics

--- src/components/api.js
+++ src/components/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({baseURL: "https://nc-news-backend-f7vf.onrender.com/api"});
 
-export const getAllArticles = () => {
-    return api.get("/articles").then(({ data })=>{
+export const getAllArticles = (query) => {
+    return api.get("/articles", query).then(({ data })=>{
         return data
     })
 }
